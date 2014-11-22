be93338ef5bff1f7f959257e40fed1a99e122f6c
Chris Pavlina <cpavlin1@binghamton.edu>
2014-11-22 10:46:01 -0500

    Completely removed old command parser

--- wcp52fw/scpi-def.c
+++ wcp52fw/scpi-def.c
@@ -41,6 +41,8 @@
 #include "scpi/scpi.h"
 #include "scpi-def.h"
 #include "asf.h"
+#include "conf_board.h"
+#include "synth.h"
 
 scpi_result_t TEST_LEDON (scpi_t * context);
 scpi_result_t TEST_LEDON (scpi_t * context) {
@@ -90,6 +92,115 @@ scpi_result_t TEST_LEDOFF (scpi_t * context) {
     }
 }
 
+scpi_result_t TEST_SETNCS (scpi_t *context);
+scpi_result_t TEST_SETNCS (scpi_t *context)
+{
+    scpi_number_t Sval;
+    if (!SCPI_ParamNumber (context, &Sval, false)) {
+        return SCPI_RES_ERR;
+    }
+    unsigned val = Sval.value;
+    if (val) {
+        gpio_set_pin_high (GPIO_SYNTH_nCS);
+    } else {
+        gpio_set_pin_low (GPIO_SYNTH_nCS);
+    }
+     return SCPI_RES_OK;   
+}
+
+scpi_result_t TEST_SETIOUP (scpi_t *context);
+scpi_result_t TEST_SETIOUP (scpi_t *context)
+{
+    scpi_number_t Sval;
+    if (!SCPI_ParamNumber (context, &Sval, false)) {
+        return SCPI_RES_ERR;
+    }
+    unsigned val = Sval.value;
+    if (val) {
+        gpio_set_pin_high (GPIO_SYNTH_IOUPDATE);
+    } else {
+        gpio_set_pin_low (GPIO_SYNTH_IOUPDATE);
+    }
+     return SCPI_RES_OK;   
+}
+
+scpi_result_t TEST_SETPWRDN (scpi_t *context);
+scpi_result_t TEST_SETPWRDN (scpi_t *context)
+{
+    scpi_number_t Sval;
+    if (!SCPI_ParamNumber (context, &Sval, false)) {
+        return SCPI_RES_ERR;
+    }
+    unsigned val = Sval.value;
+    if (val) {
+        gpio_set_pin_high (GPIO_SYNTH_PWRDN);
+    } else {
+        gpio_set_pin_low (GPIO_SYNTH_PWRDN);
+    }
+     return SCPI_RES_OK;   
+}
+
+scpi_result_t TEST_SETMRST (scpi_t *context);
+scpi_result_t TEST_SETMRST (scpi_t *context)
+{
+    scpi_number_t Sval;
+    if (!SCPI_ParamNumber (context, &Sval, false)) {
+        return SCPI_RES_ERR;
+    }
+    unsigned val = Sval.value;
+    if (val) {
+        gpio_set_pin_high (GPIO_SYNTH_MRST);
+    } else {
+        gpio_set_pin_low (GPIO_SYNTH_MRST);
+    }
+     return SCPI_RES_OK;   
+}
+
+scpi_result_t TEST_SPI (scpi_t *context);
+scpi_result_t TEST_SPI (scpi_t *context)
+{
+    scpi_number_t Sval;
+    if (!SCPI_ParamNumber (context, &Sval, false)) {
+        return SCPI_RES_ERR;
+    }
+    uint8_t val = Sval.value;
+    printf ("Transmitting value %02x\r\n", val);
+    spi_write (SPI_MASTER_BASE, val, 0, 0);
+    return SCPI_RES_OK;   
+}
+
+scpi_result_t TEST_INIF (scpi_t *context);
+scpi_result_t TEST_INIF (scpi_t *context)
+{
+    (void) context;
+    synth_initialize_interface ();
+    return SCPI_RES_OK;   
+}
+
+scpi_result_t TEST_INCK (scpi_t *context);
+scpi_result_t TEST_INCK (scpi_t *context)
+{
+    (void) context;
+    synth_initialize_clock ();
+    return SCPI_RES_OK;   
+}
+
+scpi_result_t TEST_FREQ (scpi_t *context);
+scpi_result_t TEST_FREQ (scpi_t *context)
+{
+    scpi_number_t SNchannel, SNfreq;
+    if (!SCPI_ParamNumber (context, &SNchannel, false)) {
+        return SCPI_RES_ERR;
+    }
+    if (!SCPI_ParamNumber (context, &SNfreq, false)) {
+        return SCPI_RES_ERR;
+    }
+    unsigned ch = SNchannel.value;
+    printf ("Setting frequency %d to %f\r\n", ch, SNfreq.value);
+    synth_set_frequency (ch, SNfreq.value);
+    return SCPI_RES_OK;   
+}
+
 static const scpi_command_t scpi_commands[] = {
     /* IEEE Mandated Commands (SCPI std V1999.0 4.1.1) */
     { .pattern = "*CLS", .callback = SCPI_CoreCls,},
@@ -127,6 +238,14 @@ static const scpi_command_t scpi_commands[] = {
     /* Test commands */
     {.pattern = "Test:LEDON", .callback = TEST_LEDON,},
     {.pattern = "Test:LEDOFF", .callback = TEST_LEDOFF,},
+    {.pattern = "Test:SETNCS", .callback = TEST_SETNCS,},
+    {.pattern = "Test:SETIOUP", .callback = TEST_SETIOUP,},
+    {.pattern = "Test:SETPWRDN", .callback = TEST_SETPWRDN,},
+    {.pattern = "Test:SETMRST", .callback = TEST_SETMRST,},
+    {.pattern = "Test:SPI", .callback = TEST_SPI,},
+    {.pattern = "Test:INIF", .callback = TEST_INIF,}, /* Init interface */
+    {.pattern = "Test:INCK", .callback = TEST_INCK,}, /* Init clock */
+    {.pattern = "Test:FREQ", .callback = TEST_FREQ,},
 
     SCPI_CMD_LIST_END
 };
